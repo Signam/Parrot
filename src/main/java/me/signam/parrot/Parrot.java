7badaeca66c81de3df742c3c9c06cd7b79e495af
CaptainRexPL <95997627+CaptainRexPL@users.noreply.github.com>
2023-02-04 14:02:07 +0100

    A little change

--- src/main/java/me/signam/parrot/Parrot.java
+++ src/main/java/me/signam/parrot/Parrot.java
@@ -1,115 +1,54 @@
 package me.signam.parrot;
 
-import discord4j.common.util.Snowflake;
-import discord4j.core.DiscordClient;
-import discord4j.core.GatewayDiscordClient;
-import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.Guild;
-import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.channel.GuildMessageChannel;
-import discord4j.core.spec.EmbedCreateSpec;
-import discord4j.core.spec.MessageCreateSpec;
-import discord4j.rest.util.Color;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
+public class ConfigUtils {
 
-public class Parrot {
+    private final Properties properties = new Properties();
+    private final String configFile = "config.properties";
+    private final File file = new File(configFile);
 
+    public ConfigUtils() {
 
-    public static void main(final String[] args) {
+        try {
+            if (!file.exists())
+                file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        ConfigUtils configUtils = new ConfigUtils();
+        try(FileInputStream fileInputStream = new FileInputStream(configFile)) {
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        final String token = configUtils.getToken();
-        final DiscordClient client = DiscordClient.create(token);
-        final GatewayDiscordClient gateway = client.login().block();
-
-        gateway.on(MessageCreateEvent.class).subscribe(event -> {
-
-            try {
-                final Message message = event.getMessage();
-                if (configUtils.getInputChannel().equals(message.getChannelId().asString())) {
-                    final Guild guild = message.getGuild().block();
-                    if (!event.getMessage().getContent().equals("") && !event.getMessage().getContent().equals("Message has been sent to channel <#" + configUtils.getOutputChannel() + ">")) {
-
-                        EmbedCreateSpec embed = EmbedCreateSpec.builder()
-                                .color(Color.BLUE)
-                                .title("Parrot")
-                                .description("Parrot says: " + event.getMessage().getContent())
-                                //.image("https://avatars.githubusercontent.com/u/90117558?s=200&v=4")
-                                .build();
-
-
-                        EmbedCreateSpec debugembed = EmbedCreateSpec.builder()
-                                .color(Color.BLUE)
-                                .title("Parrot")
-                                .description("Message has been sent to channel <#" + configUtils.getOutputChannel() + ">")
-                                //.image("https://avatars.githubusercontent.com/u/90117558?s=200&v=4")
-                                .build();
-
-
-                        if (!event.getMessage().getContent().equals("<@" + configUtils.getBotID() + ">")) {
-                            if (configUtils.isDebugMode()) {
-
-                                guild.getChannelById(Snowflake.of(configUtils.getInputChannel()))
-                                        .ofType(GuildMessageChannel.class)
-                                        .flatMap(channel -> channel.createMessage(MessageCreateSpec.builder()
-                                                .addEmbed(debugembed)
-                                                .build()
-                                        )).subscribe();
-                            }
-
-                            guild.getChannelById(Snowflake.of(configUtils.getOutputChannel()))
-                                    .ofType(GuildMessageChannel.class)
-                                    .flatMap(channel -> channel.createMessage(embed))
-                                    .subscribe();
-                        }
-
-
-                        event.getMessage().getData().attachments().forEach(attachmentData -> {
-
-                            EmbedCreateSpec imageembed = EmbedCreateSpec.builder()
-                                    .color(Color.BLUE)
-                                    .title("Parrot")
-                                    .description("Parrot sent a new image!")
-                                    .image(attachmentData.url())
-                                    .build();
-
-                            EmbedCreateSpec debugimageembed = EmbedCreateSpec.builder()
-                                    .color(Color.BLUE)
-                                    .title("Parrot")
-                                    .description("Image has been sent to channel <#" + configUtils.getOutputChannel() + ">")
-                                    .image(attachmentData.url())
-                                    .build();
-
-                            if (configUtils.isDebugMode()) {
-                                guild.getChannelById(Snowflake.of(configUtils.getInputChannel()))
-                                        .ofType(GuildMessageChannel.class)
-                                        .flatMap(channel -> channel.createMessage(MessageCreateSpec.builder()
-                                                .addEmbed(debugimageembed)
-                                                .build()
-                                        )).subscribe();
-                            }
-
-
-                            guild.getChannelById(Snowflake.of(configUtils.getOutputChannel()))
-                                    .ofType(GuildMessageChannel.class)
-                                    .flatMap(channel -> channel.createMessage(MessageCreateSpec.builder()
-                                            .addEmbed(imageembed)
-                                            .build()
-                                    )).subscribe();
-                        });
-
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        });
-
-        gateway.onDisconnect().block();
     }
 
+    public String getToken() {
+        return properties.get("token").toString();
+    }
 
+    //channels for sending messages
+    public String getInputChannel() {
+        return properties.get("input.channel").toString();
+    }
 
+    //channels for outputting messages
+    public String getOutputChannel() {
+        return properties.get("output.channel").toString();
+    }
+
+    //option to toggle debug mode
+    public boolean isDebugMode() {
+        return Boolean.parseBoolean(properties.getProperty("debug", "false"));
+    }
+
+    public String getBotID() {
+        return properties.get("bot.id").toString();
+    }
 }
+
