5e5e8971a8a017fc9e3b22982191c33f47903015
CaptainRexPL <95997627+CaptainRexPL@users.noreply.github.com>
2023-02-04 23:39:53 +0100

    Minor changes (#2)
    * Now Parrot sends a msg to the inputchannel as well
    
    * Create maven.yml
    
    * It was pointless
    
    * Dependabot
    
    * Delete dependabot.yml
    
    * Dependabot
    
    * Update README.md
    
    * Automated releases
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * It was pointless part 2
    
    * Update maven.yml
    
    * Buttons
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * I forgot to remove it
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Maybe I will come back to it later
    
    * Debug Mode toggling part 1
    
    * Debug Mode toggling 2
    
    * Debug Mode toggling finished
    
    * This will be important later
    
    * This will be important later 2
    
    * New message formatting
    
    * A little change
    
    * Let's see if that works...
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
+[![.github/workflows/maven.yml](https://github.com/CaptainRexPL/Parrot/actions/workflows/maven.yml/badge.svg)](https://github.com/CaptainRexPL/Parrot/actions/workflows/maven.yml)
+<img src="https://img.shields.io/github/downloads/CaptainRexPL/Parrot/total" alt="Downloads">
 # Parrot
 A bot for relaying messages from one channel to another
 
 # Instructions
-Copy config.properties.example to config.properties and fill in values.
+1. Download the latest artifact and run the command ```java -jar Parrot-1.0-SNAPSHOT-jar-with-dependencies.jar```.
+2. Fill the config.properties and run the command again
+
--- src/main/java/me/signam/parrot/Parrot.java
+++ src/main/java/me/signam/parrot/Parrot.java
@@ -6,6 +6,11 @@ import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Guild;
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.GuildMessageChannel;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.core.spec.MessageCreateSpec;
+import discord4j.rest.util.Color;
+
 
 public class Parrot {
 
@@ -19,22 +24,86 @@ public class Parrot {
         final GatewayDiscordClient gateway = client.login().block();
 
         gateway.on(MessageCreateEvent.class).subscribe(event -> {
+
             try {
                 final Message message = event.getMessage();
                 if (configUtils.getInputChannel().equals(message.getChannelId().asString())) {
-
-
                     final Guild guild = message.getGuild().block();
-                    if (!event.getMessage().getContent().equals(""))
-                        guild.getChannelById(Snowflake.of(configUtils.getOutputChannel())).block().getRestChannel()
-                                .createMessage(event.getMessage().getContent()).block();
+                    if (!event.getMessage().getContent().equals("") && !event.getMessage().getContent().equals("Message has been sent to channel <#" + configUtils.getOutputChannel() + ">")) {
 
-                    event.getMessage().getData().attachments().forEach(attachmentData -> {
-                        guild.getChannelById(Snowflake.of(configUtils.getOutputChannel())).block().getRestChannel()
-                                .createMessage(attachmentData.url()).block();
-                    });
+                        EmbedCreateSpec embed = EmbedCreateSpec.builder()
+                                .color(Color.BLUE)
+                                .title("Parrot")
+                                .description("Parrot says: " + event.getMessage().getContent())
+                                //.image("https://avatars.githubusercontent.com/u/90117558?s=200&v=4")
+                                .footer("Powered by The Fifth Column Inc Systems", "https://avatars.githubusercontent.com/u/90117558?s=200&v=")
+                                .build();
 
 
+                        EmbedCreateSpec debugembed = EmbedCreateSpec.builder()
+                                .color(Color.BLUE)
+                                .title("Parrot")
+                                .description("Message has been sent to channel <#" + configUtils.getOutputChannel() + ">")
+                                //.image("https://avatars.githubusercontent.com/u/90117558?s=200&v=4")
+                                .footer("Powered by The Fifth Column Inc Systems", "https://avatars.githubusercontent.com/u/90117558?s=200&v=")
+                                .build();
+
+
+                        if (!event.getMessage().getContent().equals("<@" + configUtils.getBotID() + ">")) {
+                            if (configUtils.isDebugMode()) {
+
+                                guild.getChannelById(Snowflake.of(configUtils.getInputChannel()))
+                                        .ofType(GuildMessageChannel.class)
+                                        .flatMap(channel -> channel.createMessage(MessageCreateSpec.builder()
+                                                .addEmbed(debugembed)
+                                                .build()
+                                        )).subscribe();
+                            }
+
+                            guild.getChannelById(Snowflake.of(configUtils.getOutputChannel()))
+                                    .ofType(GuildMessageChannel.class)
+                                    .flatMap(channel -> channel.createMessage(embed))
+                                    .subscribe();
+                        }
+
+
+                        event.getMessage().getData().attachments().forEach(attachmentData -> {
+
+                            EmbedCreateSpec imageembed = EmbedCreateSpec.builder()
+                                    .color(Color.BLUE)
+                                    .title("Parrot")
+                                    .description("Parrot sent a new image!")
+                                    .image(attachmentData.url())
+                                    .footer("Powered by The Fifth Column Inc Systems", "https://avatars.githubusercontent.com/u/90117558?s=200&v=")
+                                    .build();
+
+                            EmbedCreateSpec debugimageembed = EmbedCreateSpec.builder()
+                                    .color(Color.BLUE)
+                                    .title("Parrot")
+                                    .description("Image has been sent to channel <#" + configUtils.getOutputChannel() + ">")
+                                    .image(attachmentData.url())
+                                    .footer("Powered by The Fifth Column Inc Systems", "https://avatars.githubusercontent.com/u/90117558?s=200&v=")
+                                    .build();
+
+                            if (configUtils.isDebugMode()) {
+                                guild.getChannelById(Snowflake.of(configUtils.getInputChannel()))
+                                        .ofType(GuildMessageChannel.class)
+                                        .flatMap(channel -> channel.createMessage(MessageCreateSpec.builder()
+                                                .addEmbed(debugimageembed)
+                                                .build()
+                                        )).subscribe();
+                            }
+
+
+                            guild.getChannelById(Snowflake.of(configUtils.getOutputChannel()))
+                                    .ofType(GuildMessageChannel.class)
+                                    .flatMap(channel -> channel.createMessage(MessageCreateSpec.builder()
+                                            .addEmbed(imageembed)
+                                            .build()
+                                    )).subscribe();
+                        });
+
+                    }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
